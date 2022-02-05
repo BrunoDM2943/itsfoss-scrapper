88b2ad0e99064648d98f0b47be81cff048b49b20
BrunoDM2943 <bdm2943@gmail.com>
2022-02-05 13:27:26 -0300

    Using split

--- scrap.rb
+++ scrap.rb
@@ -3,7 +3,11 @@ require('nokogiri')
 
 html = open("https://itsfoss.com/")
 doc = Nokogiri::HTML(html)
+split = '------------------------------------------------------------'
+puts doc.title()
+puts split
 news = doc.css('h4.ugb-blog-posts__title')
 news.each { |el| 
     puts "#{el.text} - #{el.css('a')[0]['href']}"
+    puts split
 }
